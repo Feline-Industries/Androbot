267f32ec52825502195bd9cea36dd4122eba84dd
UnderScore <drojasg25@gmail.com>
2024-06-28 19:52:50 -0400

    add boops beet

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DisCatSharp;
+using DisCatSharp.Enums;
+using NuGet.Common;
+
+namespace Androbot
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration(){
+            Token = File.ReadAllText("token.txt"),
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContent
+            });
+
+            discord.MessageCreated += async (s, e) => {
+                if (e.Message.Content.ToLower().StartsWith("boop"))
+                    await e.Message.RespondAsync("beep!");
+            };
+            
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
