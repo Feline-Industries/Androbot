93f97ea3a4b9d475f777d61db64fb61a1421f0bb
UnderScore <drojasg25@gmail.com>
2024-07-19 11:50:56 -0400

    Add UserInfo Command Group

--- AndroCommands.cs
+++ AndroCommands.cs
@@ -1,13 +1,27 @@
 using Discord.Commands;
+using Discord.WebSocket;
 
 namespace Program{
-    public class Commands : ModuleBase<SocketCommandContext>
+    public class BasicInfo : ModuleBase<SocketCommandContext>
         {
         [Command("talk")]
         [Summary("cute response")]
-        public async Task Talk(){
+        public async Task TalkAsync(){
             Console.WriteLine("command used");
             await Context.Channel.SendMessageAsync("UwU");
+        }
+    }
+
+    //to probe for user's information
+    [Group("userinfo")]
+    public class UserInfo : ModuleBase<SocketCommandContext>
+    {
+        [Command("username")]
+        [Summary("to pull up a user's username & unique id")]
+        [Alias("user", "whois")]
+        public async Task UserInfoAsync([Summary("user id")] SocketUser? user = null){
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"{userInfo.Username}#{userInfo.Discriminator}");
         }
     }
 }
